af746b41f247e546d992760ce1094387aeffe2f9
Michael <michael.totarella@mail.concordia.ca>
2021-04-26 20:45:46 -0400

    Created color table; added necessary media files

---
+++ mars_orbiter.py
@@ -0,0 +1,11 @@
+import os  # to be able to escape full screen
+import math
+import random  # to randomize initial orbit and velocity
+import pygame as pg
+
+# color variables
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+LT_BLUE = (173, 216, 230)
